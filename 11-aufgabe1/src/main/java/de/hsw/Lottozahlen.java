7dd520f14a88be3d76723f12088cfc5125afe0d8
TIMBLOCKER <tim.borkowy@gmx.de>
2022-11-19 12:56:06 +0100

    Changes by huynh 15.11 17 Uhr

--- 11-aufgabe1/src/main/java/de/hsw/Lottozahlen.java
+++ 11-aufgabe1/src/main/java/de/hsw/Lottozahlen.java
@@ -3,6 +3,7 @@ package de.hsw;
 
 import com.sun.source.tree.Tree;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 
@@ -14,54 +15,43 @@ public class Lottozahlen {
 
         int[] spielRundenArray = new int[anzahl];
         Set<Integer> lottozahlenArray = null;
+
         for (int i = 0; i < spielRundenArray.length; i++) {
 
             System.out.println("DAS SIND DIE ZAHLEN FÜR DIE SPIELRUNDE " + (i + 1) + ":");
             lottozahlenArray = new TreeSet<>();
 
 
-
             while (lottozahlenArray.size() < 6) {
-                lottozahlenArray.add(rand.nextInt(1,49));
+                lottozahlenArray.add(rand.nextInt(1, 49));
 
             }
             System.out.println(lottozahlenArray.toString());
-            System.out.println("Ihre Zusatzzahl: [" + "PLATZHALTER" + "]");
+
+            int zusatzZahl = rand.nextInt(1,49);
+                while(lottozahlenArray.contains(zusatzZahl)){
+                    zusatzZahl = rand.nextInt(1,49);
+                }
+                lottozahlenArray.add(zusatzZahl);
+
+            System.out.println("Ihre Zusatzzahl: [" + zusatzZahl + "]");
 
             System.out.println(" ");
-        }
-        System.out.println("----------SUPERZAHL----------");
-        int superZahl = rand.nextInt(1,9);
-        System.out.println("Ihre Superzahl: " + superZahl);
-
-
-
-    }
-
-    public void tristanderprofi(){
-            Random random = new Random();
-
-            int arr[] = {12, 13 ,25, 24, 30, 14};
-            int zzz = 0;
-            boolean lol = false;
-            while (lol == false){
-                boolean lal = false;
-                int zz = random.nextInt(1,49);
-                for (int i = 0; i < 6; i++) {
-                    if (arr[i] == zz){
-                        lal = true;
-                        break;
-                    }
-                }
-                if (lal == false){
-                    lol = true;
-                    zzz = zz;
-                }
             }
-            System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5]);
-            System.out.println("Deine ZZ ist: " + zzz);
+
+
+        System.out.println("--------------------SUPERZAHL--------------------");
+        int superZahl = rand.nextInt(1, 9);
+        System.out.println("Ihre Superzahl: [" + superZahl + "]");
+
+
+        }
+
+
+
     }
-}
+
+
 
 
 
